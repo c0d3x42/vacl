35a63eb574c3d8f653a262afe212ca7a0c7ae9cc
root <c0d3x42@gmail.com>
2008-11-24 20:43:04 +0000

    added generator base

--- generators/vacl/vacl_generator.rb
+++ generators/vacl/vacl_generator.rb
@@ -3,6 +3,16 @@ class VaclGenerator < Rails::Generator::NamedBase
     record do |m|
       # m.directory "lib"
       # m.template 'README', "README"
+      m.migration_template 'migration:migration.rb', "db/migrate", {
+        :assigns => vacl_local_assigns,
+        :migration_file_name => "create_vacl"
+      }
     end
+  end
+
+  private
+  def vacl_local_assigns
+    returning(assigns = {}) do
+      assigns[:class_name] = "Vacl"
   end
 end

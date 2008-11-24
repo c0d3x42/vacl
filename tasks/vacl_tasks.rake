5bf2bd55d2e7150475ea284a981b5574fab29e21
root <c0d3x42@gmail.com>
2008-11-24 19:44:46 +0000

    initial layout

---
+++ tasks/vacl_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :vacl do
+#   # Task goes here
+# end
---
+++ test/vacl_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class VaclTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end

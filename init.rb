ba6f843eb1957bea551d7340e954faa96be503cc
root <c0d3x42@gmail.com>
2009-04-03 17:55:43 +0000

    set controller path

--- init.rb
+++ init.rb
@@ -6,3 +6,8 @@ ApplicationController.prepend_view_path( File.join( File.dirname( __FILE__ ), 'a
 model_path = File.join( directory, 'lib', 'app', 'models' )
 $LOAD_PATH << model_path
 ActiveSupport::Dependencies.load_paths << model_path
+
+controller_path = File.join(directory, 'lib', 'app', 'controllers')
+$LOAD_PATH.insert(0, controller_path)
+ActiveSupport::Dependencies.load_paths.insert(0, controller_path)
+config.controller_paths.insert(0, controller_path)

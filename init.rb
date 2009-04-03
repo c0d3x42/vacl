d896bbc9bdd43077ccc31ffbf61fea4df474ea74
root <c0d3x42@gmail.com>
2009-04-03 17:51:13 +0000

    set model path on the fly

--- init.rb
+++ init.rb
@@ -1,2 +1,8 @@
 # Include hook code here
 require 'acts_as_vacl'
+
+ApplicationController.prepend_view_path( File.join( File.dirname( __FILE__ ), 'app', 'views' ) )
+
+model_path = File.join( directory, 'lib', 'app', 'models' )
+$LOAD_PATH << model_path
+ActiveSupport::Dependencies.load_paths << model_path
